900613ad9a1d84120b29feff1c97dd11fa088442
Guilherme Trein <trein@amazon.com>
2015-08-26 10:40:28 +0200

    Improve project organization

--- pyconfig/loader.py
+++ pyconfig/loader.py
@@ -6,7 +6,6 @@ import json
 logger = logging.getLogger(__name__)
 
 VIRTUALENV_DIR = '.env'
-
 PROJECT_DESCRIPTOR_NAME = 'project.json'
 
 DEPENDENCIES_KEY = 'dependencies'
@@ -28,16 +27,15 @@ class DependencyDescriptor(object):
         return self.descriptor.get(DEPENDENCIES_KEY, {})
 
     def dev_dependencies(self):
-        for dep_name, dep_version in self.dependencies().get(DEPENDENCIES_DEV_KEY, {}).items:
+        for dep_name, dep_version in self.dependencies().get(DEPENDENCIES_DEV_KEY, {}).items():
+            yield dep_name, dep_version
+
+    def prod_dependencies(self):
+        for dep_name, dep_version in self.dependencies().get(DEPENDENCIES_PROD_KEY, {}).items():
             yield dep_name, dep_version
 
 
 class Installer(object):
-    def clean_env(self):
-        logger.info('Cleaning virtualenv at %s' % VIRTUALENV_DIR)
-        os.system('virtualenv %s' % VIRTUALENV_DIR)
-        os.system('source %s/bin/activate' % VIRTUALENV_DIR)
-
     def disable_env(self):
         logger.info('Disabling virtualenv at %s' % VIRTUALENV_DIR)
         os.system('source %s/bin/deactivate' % VIRTUALENV_DIR)
@@ -49,6 +47,10 @@ class Installer(object):
     def setup_env(self):
         logger.info('Setting up virtualenv at %s' % VIRTUALENV_DIR)
         os.system('virtualenv %s' % VIRTUALENV_DIR)
+
+    def clean_env(self):
+        logger.info('Cleaning virtualenv at %s' % VIRTUALENV_DIR)
+        os.system('rm -r %s' % VIRTUALENV_DIR)
 
     def is_dependency_satisfied(self, name, version):
         import pkg_resources
@@ -71,30 +73,56 @@ class Workflow(object):
 
 class CleanWorkflow(Workflow):
     def do(self):
-        Installer().clean_env()
+        installer = Installer()
+        installer.disable_env()
+        installer.clean_env()
 
 
-class DependencyWorkflow(Workflow):
+class ProdDependencyWorkflow(Workflow):
     def __init__(self, args, descriptor):
-        super(DependencyWorkflow, self).__init__()
+        super(ProdDependencyWorkflow, self).__init__()
         self.args = args
         self.dep_descriptor = DependencyDescriptor(descriptor)
         self.installer = Installer()
 
+    def _install_prod_dependencies(self):
+        for dep_name, dep_version in self.dep_descriptor.dev_dependencies():
+            logger.info('Processing dependency %s%s' % (dep_name, dep_version))
+            self.installer.install_dependency(dep_name, dep_version)
+
     def do(self):
         self.installer.setup_env()
-        for dep_name, dep_version in self.dep_descriptor.dev_dependencies():
+        self._install_prod_dependencies()
+        self.installer.enable_env()
+
+
+class DevDependencyWorkflow(ProdDependencyWorkflow):
+    def __init__(self, args, descriptor):
+        super(DevDependencyWorkflow, self).__init__(args, descriptor)
+
+    def _install_dev_dependencies(self):
+        for dep_name, dep_version in self.dep_descriptor.prod_dependencies():
+            logger.info('Processing dependency %s%s' % (dep_name, dep_version))
             self.installer.install_dependency(dep_name, dep_version)
+
+    def do(self):
+        self.installer.setup_env()
+        self._install_dev_dependencies()
+        self._install_prod_dependencies()
         self.installer.enable_env()
 
 
 def setup_project(args):
     descriptor = read_project_descriptor()
-    if args.clean:
+    logger.info('Executing %s workflow' % args.mode)
+    if args.mode == 'clean':
         CleanWorkflow().do()
         return
-    if args.release:
-        DependencyWorkflow(args, descriptor).do()
+    if args.mode == 'release':
+        ProdDependencyWorkflow(args, descriptor).do()
+        return
+    if args.mode == 'develop':
+        DevDependencyWorkflow(args, descriptor).do()
         return
 
 
---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(name='pyconfig',
+      version='0.1.0',
+      description='Unified development workflow automation tool',
+      author='Guilherme Trein',
+      author_email='guitrein@yahoo.com.br',
+      url='https://github.com/trein/pyconfig',
+      license='Apache License 2.0',
+      long_description=open('README.md').read(),
+      platforms='all',
+      packages=['pyconfig'])
